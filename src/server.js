7acd18a1e22c5978aa3ce22e788b7cfb19e30332
Comdev2k23 <comdev084@gmail.com>
2025-07-19 21:58:54 +0800

    updating

--- src/server.js
+++ src/server.js
@@ -3,17 +3,15 @@ import dotenv from 'dotenv'
 import { initDB } from './config/db.js'
 import transactionRouter from './routes/transactions.routes.js'
 import userRouter from './routes/users.routes.js'
-import job from './config/cron.js'
-import rateLimiter from './middlewares/rateLimiter.js'
+
 
 dotenv.config()
 
 const app = express()
-// if (process.env.NODE_ENV === "production") job.start();
+
 
 //Middleware
 app.use(express.json())
-app.use(rateLimiter);
 
 const PORT = process.env.PORT || 5001
 
