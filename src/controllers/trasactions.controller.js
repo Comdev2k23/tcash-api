6f011f835de5ba8abd10bbb8cc5cc149eecd508d
Comdev2k23 <comdev084@gmail.com>
2025-07-22 14:36:20 +0800

    updating

--- src/controllers/trasactions.controller.js
+++ src/controllers/trasactions.controller.js
@@ -8,10 +8,7 @@ export async function getTransactionByUserId (req, res, next) {
             SELECT * FROM transactions WHERE user_id = ${userId}
             ORDER BY created_at DESC 
         `
-        if(transactions.length === 0){
-            return res.status(404).json({message: 'Transaction/s not found'})
-        }
-
+       
         res.status(200).json(transactions)
 
     } catch (error) {
